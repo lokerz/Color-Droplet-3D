749e6e1220d05ad0ded0900903f411716d9266f1
LokerZ <ridwan.abdurrasyid@yahoo.com>
2018-11-23 06:49:58 +0700

    v1.1

--- Assets/Scripts/RingRotate.cs
+++ Assets/Scripts/RingRotate.cs
@@ -11,22 +11,17 @@ public class RingRotate : MonoBehaviour {
 	void Start(){
 		rb = GetComponent<Rigidbody> ();
 	}
-
-	//#if UNITY_EDITOR
+		
 	void OnMouseDown(){
-		Debug.Log ("called");
+		
 		rb.angularVelocity = Vector3.zero;
 	}
 
 	void OnMouseDrag(){
 		float Rotation = Input.GetAxis ("Mouse X") * rotationSpeed;
-		//transform.RotateAround (Vector3.down, Rotation);
+
 		rb.AddTorque (transform.up * -Rotation);
 	}
-	//#endif
 
-	//#if UNITY_ANDROID
-
-	//#endif
 
 }
